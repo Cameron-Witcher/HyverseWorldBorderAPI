7d038f42254508d39841f473295df18479a5a6ea
Cameron-Witcher <admin@hyverse.network>
2019-12-16 07:15:20 -0600

    Added 1.14 compatability

--- src/com/github/yannicklamprecht/worldborder/WorldBorderPlugin.java
+++ src/com/github/yannicklamprecht/worldborder/WorldBorderPlugin.java
@@ -10,24 +10,27 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 public class WorldBorderPlugin extends JavaPlugin {
 
-    @Override
-    public void onEnable() {
+	@Override
+	public void onEnable() {
 
-        WorldBorderApi worldBorderApi;
+		WorldBorderApi worldBorderApi;
 
-        String version = Bukkit.getServer().getClass().getPackage().getName().replace('.',',').split(",")[3];
-        System.out.println("Version: "+ version);
-        switch (version) {
-            
-            case "v1_15_R1":
-                worldBorderApi = new com.github.yannicklamprecht.worldborder.v1_15_R1.Impl();
-                break;
-            default: {
-                getLogger().info("Unsupported version of Minecraft");
-                Bukkit.getPluginManager().disablePlugin(this);
-                return;
-            }
-        }
-        BorderAPI.setWorldBorderApi(worldBorderApi);
-    }
+		String version = Bukkit.getServer().getClass().getPackage().getName().replace('.', ',').split(",")[3];
+		System.out.println("Version: " + version);
+		switch (version) {
+		case "v1_14_R1":
+			worldBorderApi = new com.github.yannicklamprecht.worldborder.v1_14_R1.Impl();
+			break;
+
+		case "v1_15_R1":
+			worldBorderApi = new com.github.yannicklamprecht.worldborder.v1_15_R1.Impl();
+			break;
+		default: {
+			getLogger().info("Unsupported version of Minecraft");
+			Bukkit.getPluginManager().disablePlugin(this);
+			return;
+		}
+		}
+		BorderAPI.setWorldBorderApi(worldBorderApi);
+	}
 }
